36257a8f58f8cd897bdc50c40807eb4b6b330f6b
Jeanykay Simon <jkaysym03@gmail.com>
2017-11-08 00:47:38 +0000

    classwork

---
+++ reverseAstring.js
@@ -0,0 +1,20 @@
+function reverseString(str) {
+ var StrArray = ;
+ 
+ strArray = str.split("");
+ 
+//  console.log("splitting the string:");
+//  console.log(strArray);
+ 
+ strArray.reverse();
+ 
+//  console.log("after reverse");
+//  console.log(strArray);
+ 
+var reverseStr = strArray.join("");
+// console.log('after join');
+// console.log(reverseStr);
+
+console.log(reverseString("Jeanykay Simon")
+ 
+ 
